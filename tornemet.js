69b1275aa452de36ba88545ea6a3285826fcbbbf
karamnaser <karam_m_n@hotmail.com>
2019-12-26 12:07:43 +0200

    adding phase 3

--- tornemet.js
+++ tornemet.js
@@ -3,6 +3,7 @@ let Game = require('./game');
 let EvenOdd = require('./EvenOdd');
 let players = [];
 let winer = null;
+boss = { name: "the boss", score: 0 };
 let currentplayer = [];
 
 function creatPlayers(num_of_players = 2) {
@@ -22,11 +23,31 @@ function playinguntil3wins() {
         creatPlayers(2);
         choosePlayerForRound(players);
         EvenOdd.startingGame(currentplayer)
-        winer = currentplayer.find(player => players.total_wins == 3);
+        winer = currentplayer.find(player => player.total_wins == 3);
         console.log(currentplayer[0].total_wins, currentplayer[1].total_wins)
         currentplayer = [];
     }
     console.log(winer)
+}
+
+function playingagainstBoss() {
+    Game.totale_games = 5;
+    for (let i = 1; i <= Game.totale_games; i++) {
+        let num = Game.generateNumber();
+        if (num % 2 == 0 && num > 0) {
+            Game.increasScore(winer)
+            console.log(`Round ${i}, random number is ${num}, ${winer.name} scored!
+            `, ` Status : ${winer.name} : ${winer.score} ${boss.name} : ${boss.score}`)
+        } else if (num < 0) {
+            i--
+            continue;
+        } else {
+            Game.increasScore(players[1])
+            console.log(`Round ${i}, random number is ${num}, ${winer.name} scored!
+            `, ` Status : ${winer.name} : ${winer.score} ${boss.name} : ${boss.score}`)
+        }
+    }
+    Game.checkWinner([players[0], players[1]]);
 }
 
 playinguntil3wins();
